f10c85dac25de431178f7196bacf9e419a43adc5
Aldren Terante <aldren.terante@salarium.com>
2015-12-21 19:27:10 +0800

    add default basic authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('/', ['middleware' => 'auth', function() {
     return view('index');
-});
+}]);
